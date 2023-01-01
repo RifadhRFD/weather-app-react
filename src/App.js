2cf085c550f6ff20db7de2d43d370a0ad2327e33
RifadhRFD <rifadhrfd@gmail.com>
2023-01-01 23:32:46 +0530

    basic conf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
 import './App.css';
 
+const api = {
+  key: "dc75625ce33317bad2a1130af7d7b949",
+  base: "http://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
+
   return (
-    <div className="App">
-      <h1>weather app</h1>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search ..." />
+        </div>
+      </main>
     </div>
   );
 }
